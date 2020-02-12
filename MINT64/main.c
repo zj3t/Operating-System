8d2d8ecb830fb75ae372ff0595856639671f9b81
yibyeongyong <51373248+yibyeongyong@users.noreply.github.com>
2020-02-12 18:04:43 +0900

    Day1-Bootloader_Essential

---
+++ MINT64/main.c
@@ -0,0 +1,10 @@
+//
+// Created by yby-lab on 2020-02-12.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
---
+++ MINT64/qemu.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\qemu\qemu-system-x86_64.exe" -L . -boot c -m 256 "C:\Users\yby-lab\CLionProjects\Operating-System\MINT64\Disk.img"
