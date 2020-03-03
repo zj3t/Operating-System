d66f52e6eb335195cc1cd4ebfe85d144cbd6f908
zj3t <euntaejang@gmail.com>
2020-03-03 03:16:37 -0800

    08_A20GATE

--- 04.Utility/00.ImageMaker/ImageMaker.c
+++ 04.Utility/00.ImageMaker/ImageMaker.c
@@ -1,16 +1,6 @@
-/**
- *  file    ImageMaker.c
- *  date    2008/12/16
- *  author  kkamagui 
- *          Copyright(c)2008 All rights reserved by kkamagui
- *  brief   부트 로더와 커널 이미지를 연결하고, 섹터 단위로 정렬해 주는 ImageMaker의 
- *          소스 파일
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <io.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -41,8 +31,7 @@ int main(int argc, char* argv[])
     }
     
     // Disk.img 파일을 생성
-    if( ( iTargetFd = open( "Disk.img", O_RDWR | O_CREAT |  O_TRUNC |
-            O_BINARY, S_IREAD | S_IWRITE ) ) == -1 )
+    if( ( iTargetFd = open( "Disk.img", O_RDWR | O_CREAT |  O_TRUNC ,0664 ) ) == -1 )
     {
         fprintf( stderr , "[ERROR] Disk.img open fail.\n" );
         exit( -1 );
@@ -52,7 +41,7 @@ int main(int argc, char* argv[])
     // 부트 로더 파일을 열어서 모든 내용을 디스크 이미지 파일로 복사
     //--------------------------------------------------------------------------
     printf( "[INFO] Copy boot loader to image file\n" );
-    if( ( iSourceFd = open( argv[ 1 ], O_RDONLY | O_BINARY ) ) == -1 )
+    if( ( iSourceFd = open( argv[ 1 ], O_RDONLY ) ) == -1 )
     {
         fprintf( stderr, "[ERROR] %s open fail\n", argv[ 1 ] );
         exit( -1 );
@@ -70,7 +59,7 @@ int main(int argc, char* argv[])
     // 32비트 커널 파일을 열어서 모든 내용을 디스크 이미지 파일로 복사
     //--------------------------------------------------------------------------
     printf( "[INFO] Copy protected mode kernel to image file\n" );
-    if( ( iSourceFd = open( argv[ 2 ], O_RDONLY | O_BINARY ) ) == -1 )
+    if( ( iSourceFd = open( argv[ 2 ], O_RDONLY ) ) == -1 )
     {
         fprintf( stderr, "[ERROR] %s open fail\n", argv[ 2 ] );
         exit( -1 );
--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
-# 기본적으로 빌드를 수행할 목록
-all: BootLoader Disk.img
+all: BootLoader Kernel32 Disk.img Utility
 
 # 부트 로더 빌드를 위해 부트 로더 디렉터리에서 make 실행
 BootLoader:
@@ -12,20 +11,47 @@ BootLoader:
 	@echo 
 	@echo =============== Build Complete ===============
 	@echo 
+
+# 가상 OS 이미지 빌드를 위해 보호 모드 커널 디렉터리에서 make 실행
+Kernel32:
+	@echo 
+	@echo ============== Build 32bit Kernel ===============
+	@echo 
 	
+	make -C 01.Kernel32
+
+	@echo 
+	@echo =============== Build Complete ===============
+	@echo 
+
+
 # OS 이미지 생성
-Disk.img: 00.BootLoader/BootLoader.bin
+Disk.img: 00.BootLoader/BootLoader.bin 01.Kernel32/Kernel32.bin Utility
 	@echo 
 	@echo =========== Disk Image Build Start ===========
 	@echo 
 
-	cp 00.BootLoader/BootLoader.bin Disk.img
+	./04.Utility/00.ImageMaker/ImageMaker.exe $^
 
 	@echo 
 	@echo ============= All Build Complete =============
 	@echo 
-	
+
+# 유틸리티 빌드
+Utility:
+	@echo 
+	@echo =========== Utility Build Start ===========
+	@echo 
+
+	make -C 04.Utility
+
+	@echo 
+	@echo =========== Utility Build Complete ===========
+	@echo 
+
 # 소스 파일을 제외한 나머지 파일 정리	
 clean:
 	make -C 00.BootLoader clean
+	make -C 01.Kernel32 clean
+	make -C 04.Utility clean
 	rm -f Disk.img	
